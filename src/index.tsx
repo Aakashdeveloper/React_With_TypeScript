336f7771f550173077282e4b0dbb4b519c011ac5
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-05 23:33:55 +0530

    a

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './components/App'
+import "./styles/style.scss";
+
+const ROOT = document.getElementById("root");
+
+ReactDOM.render(<App/>, ROOT);
